fcabab8dfd3aba542cec78ce4866985dc058fc3a
XiaoxuHU <tjhuxiaoxu@gmail.com>
2020-10-14 12:54:24 -0700

    Add Error Msg for empty uri

--- AngularApp/projects/app-service-diagnostics/src/app/home/resolvers/resource.resolver.ts
+++ AngularApp/projects/app-service-diagnostics/src/app/home/resolvers/resource.resolver.ts
@@ -21,15 +21,14 @@ export class ResourceResolver implements Resolve<Observable<{} | ArmResource>> {
             .filter(x => x.path !== 'new' && x.path !== 'categories')
             .map(x => x.path)
             .join('/');
-        if (this.checkResourceUri(resourceUri)) {
+        if (!this.checkResourceUri(resourceUri)) {
             const url = state.url;
             const startIndex = url.indexOf("subscriptions/") > -1 ? url.indexOf("subscriptions/") : 0;
             const endIndex = url.indexOf("/categories") > -1 ? url.indexOf("/categories") : url.length;
 
-            resourceUri = url.substring(startIndex,endIndex);
+            resourceUri = url.substring(startIndex, endIndex);
         }
-
-        if(this.checkResourceUri(resourceUri)){
+        if (!this.checkResourceUri(resourceUri)) {
             const urlFromActivatedRoute = activatedRouteSnapshot.parent.url.join('/');
             throw new Error(`Empty ResourceUri from resource.resolver;Activated Route: ${urlFromActivatedRoute}; State Url: ${state.url}`);
         }
@@ -37,7 +36,7 @@ export class ResourceResolver implements Resolve<Observable<{} | ArmResource>> {
         return this._resourceService.registerResource(resourceUri);
     }
 
-    private checkResourceUri(resourceUri:string):boolean{
-        return resourceUri === "" || resourceUri === "/";
+    private checkResourceUri(resourceUri: string): boolean {
+        return resourceUri !== "" && resourceUri !== "/";
     }
 }
