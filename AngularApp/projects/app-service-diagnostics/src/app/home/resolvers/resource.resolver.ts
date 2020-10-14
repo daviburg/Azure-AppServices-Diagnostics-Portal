b6a94707589cf7e84fc4069bed77b157c193ccf4
XiaoxuHU <tjhuxiaoxu@gmail.com>
2020-10-14 12:49:26 -0700

    Add Error Message

--- AngularApp/projects/app-service-diagnostics/src/app/home/resolvers/resource.resolver.ts
+++ AngularApp/projects/app-service-diagnostics/src/app/home/resolvers/resource.resolver.ts
@@ -21,13 +21,23 @@ export class ResourceResolver implements Resolve<Observable<{} | ArmResource>> {
             .filter(x => x.path !== 'new' && x.path !== 'categories')
             .map(x => x.path)
             .join('/');
-        if (resourceUri === "" || resourceUri === "/") {
+        if (this.checkResourceUri(resourceUri)) {
             const url = state.url;
             const startIndex = url.indexOf("subscriptions/") > -1 ? url.indexOf("subscriptions/") : 0;
             const endIndex = url.indexOf("/categories") > -1 ? url.indexOf("/categories") : url.length;
 
             resourceUri = url.substring(startIndex,endIndex);
         }
+
+        if(this.checkResourceUri(resourceUri)){
+            const urlFromActivatedRoute = activatedRouteSnapshot.parent.url.join('/');
+            throw new Error(`Empty ResourceUri from resource.resolver;Activated Route: ${urlFromActivatedRoute}; State Url: ${state.url}`);
+        }
+
         return this._resourceService.registerResource(resourceUri);
+    }
+
+    private checkResourceUri(resourceUri:string):boolean{
+        return resourceUri === "" || resourceUri === "/";
     }
 }
